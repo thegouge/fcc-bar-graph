ca85cb7631c7d786a15679e0a506c2ec47c274f5
thegouge <thegouge42@gmail.com>
2019-04-21 14:54:40 -0700

    added axes to graph (trying to fix scaling issues)

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ getData().then((data) => {
   graphData(data.data);
 });
 
-const width = 1000;
-const height = 500;
 const padding = 50;
+const width = 1000;
+const height = 461;
 
 function graphData(data) {
   const xScale = d3
@@ -24,12 +24,16 @@ function graphData(data) {
 
   const yScale = d3
     .scaleLinear()
-    .domain([0, d3.max(data, (d) => d[1])])
-    .range([padding, height - padding]);
+    .domain([d3.max(data, (d) => d[1]), 0])
+    .range([height - padding, padding]);
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
 
   const svg = d3
-    .select("body")
+    .select("section")
     .append("svg")
+    .attr("class", "graph")
     .attr("width", width)
     .attr("height", height);
 
@@ -38,9 +42,22 @@ function graphData(data) {
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => i * 2)
-    .attr("y", (d) => height - d[1] / 50)
-    .attr("width", 2)
-    .attr("height", (d) => yScale(d[1]))
+    .attr("class", "bar")
+    .attr("x", (d, i) => i * 2 + padding)
+    .attr("y", (d) => height - padding - d[1] / 50)
+    .attr("width", () => width / data.length)
+    .attr("height", (d) => yScale(d[1]) - padding)
     .attr("fill", "blue");
+
+  svg
+    .append("g")
+    .attr("id", "x-axis")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .call(xAxis);
+
+  svg
+    .append("g")
+    .attr("id", "y-axis")
+    .attr("transform", "translate(" + padding + " , 0)")
+    .call(yAxis);
 }
